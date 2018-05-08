9febd2b066b164e51a38ed2f2550afceb8e76119
Irving Santiago <isantiago@homeaway.com>
2018-05-08 09:08:20 -0500

    Add brew install script

---
+++ brew_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brew packages
+brew install autoconf git libevent lz4 pkg-config tmux automake giter8 libidn2 lzlib python cassandra go librdkafka maven python3 unibilium cmake gradle libtermkey msgpack readline vim ctags grep libtool neovim reattach-to-user-namespace wget curl icu4c libunistring node ruby xz cython jemalloc libuv oniguruma sbt zookeeper elasticsearch jq libvterm openssl scala zsh gdbm kafka libyaml pcre sqlite gettext kotlin luajit perl the_silver_searcher 
