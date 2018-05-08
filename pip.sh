956771e5b42ebba196188efb670aee3aee7620ab
Irving Santiago <isantiago@homeaway.com>
2018-05-08 10:40:38 -0500

    Add pip script

---
+++ pip.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+pip3 install appnope bleach cycler decorator entrypoints greenlet html5lib ipykernel ipython ipython-genutils ipywidgets jedi Jinja2 jsonschema jupyter jupyter-client jupyter-console jupyter-core MarkupSafe matplotlib mistune msgpack-python nbconvert nbformat neovim notebook numpy pandas pandocfilters parso pexpect pickleshare pip prompt-toolkit ptyprocess Pygments pyparsing python-dateutil pytz pyzmq qtconsole setuptools sexpdata simplegeneric six terminado testpath tornado traitlets wcwidth webencodings websocket-client wheel widgetsnbextension
